081d5eeae414f9d924e26e3bbb774a1498729aeb
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-09 11:50:22 +0000

    Add simple conditional for fizzbuzz

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to this FizzBuzz program!");
+Console.WriteLine("Enter a number to be checked:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 3 == 0 && number % 5 == 0) {
+  Console.WriteLine("FizzBuzz");
+} else if (number % 3 == 0) {
+  Console.WriteLine("fizz");
+} else if (number % 5 == 0) {
+  Console.WriteLine("buzz");
+} else {
+  Console.WriteLine(number);
+}
