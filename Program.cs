0154d740d2e096c4c89d5173d5d0b974b4050f9e
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-09 15:19:03 +0000

    Create namespace and program class with Main function

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this FizzBuzz program!");
-Console.WriteLine("Enter a number to be checked:");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 3 == 0 && number % 5 == 0) {
-  Console.WriteLine("FizzBuzz");
-} else if (number % 3 == 0) {
-  Console.WriteLine("fizz");
-} else if (number % 5 == 0) {
-  Console.WriteLine("buzz");
-} else {
-  Console.WriteLine(number);
+﻿namespace FizzBuzzNS
+{
+  class Program
+  {
+    static void Main()
+    {
+      FizzBuzz();
+    }
+
+    static void FizzBuzz()
+    {
+      Console.WriteLine("Welcome to this FizzBuzz program!");
+      Console.WriteLine("Enter a number to be checked:");
+      int number = Convert.ToInt32(Console.ReadLine());
+      if (number % 3 == 0 && number % 5 == 0)
+      {
+        Console.WriteLine("FizzBuzz");
+      }
+      else if (number % 3 == 0)
+      {
+        Console.WriteLine("fizz");
+      }
+      else if (number % 5 == 0)
+      {
+        Console.WriteLine("buzz");
+      }
+      else
+      {
+        Console.WriteLine(number);
+      }
+    }
+  }
 }
