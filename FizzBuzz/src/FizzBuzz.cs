92a3527aeb4edfde96844278029f4250c3f426e2
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-16 21:36:50 +0000

    Add curly brackets in if statements

--- FizzBuzz/src/FizzBuzz.cs
+++ FizzBuzz/src/FizzBuzz.cs
@@ -6,10 +6,10 @@ namespace FizzBuzz
   {
     public string Check(int num)
     {
-      if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
-      else if (num % 3 == 0) return "Fizz";
-      else if (num % 5 == 0) return "Buzz";
-      return $"{num}";
+      if (num % 3 == 0 && num % 5 == 0) { return "FizzBuzz"; }
+      else if (num % 3 == 0) { return "Fizz"; }
+      else if (num % 5 == 0) { return "Buzz"; }
+      else { return $"{num}"; }
     }
   }
 }
