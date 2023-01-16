990ee1d0c3f14a6c357ac0afad0bb15f97f15d9f
jmcnally17 <joe.t.mcnally@hotmail.com>
2023-01-16 17:43:45 +0000

    Add checker into Main
    Create the interface and check the number inputted by the user by using
    the FizzBuzzChecker class.

--- FizzBuzz/src/Program.cs
+++ FizzBuzz/src/Program.cs
@@ -6,7 +6,11 @@ namespace FizzBuzz
   {
     static void Main()
     {
+      var fizzBuzzChecker = new FizzBuzzChecker();
       Console.WriteLine("Welcome to this FizzBuzz program!");
+      Console.WriteLine("Please enter a number to be checked:");
+      int number = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(fizzBuzzChecker.Check(number));
     }
   }
 }
